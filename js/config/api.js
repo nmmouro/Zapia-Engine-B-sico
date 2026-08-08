cc7184e0758f4c99599e7a7374bce4d922e54d33
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-08 09:17:17 -0300

    Update api.js

--- js/config/api.js
+++ js/config/api.js
@@ -1,4 +1,4 @@
 export const API_URL =
-  "https://script.google.com/macros/s/AKfycbzkSLfRlpiLfiPILIDi_JJ7kiDduAbpfZXJPOLq5Sd5_DRtK7BsoOzsZ7arJIQcCNsxyQ/exec";
+  "https://script.google.com/macros/s/AKfycbw_VByxWwWEgZVny6s2CyLJUR4P1y_7OKc_ujwVEcnxxob4hxxSrdvIQMzWiMmwmIIHsw/exec";
 
 export const API_TIMEOUT = 15000;
