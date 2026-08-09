97d9a8f305ad216cf6c4dd137a179c10345448e7
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-09 19:07:47 -0300

    Create lancamentos.helpers.js

---
+++ js/pages/lancamentos/lancamentos.helpers.js
@@ -0,0 +1,111 @@
+// ============================================================================
+// EMPREGADOS HELPERS
+// Arquivo: js/pages/lancamentos/lancamentos.helpers.js
+// ============================================================================
+
+import {
+    obterEmpregados
+} from "../../services/lancamentos.service.js";
+
+import {
+    renderTable
+} from "../../ui/table.js";
+
+import {
+    definirRegistros
+} from "./lancamentos.state.js";
+
+// ============================================================================
+// COLUNAS
+// ============================================================================
+
+export const COLUNAS_LANCAMENTOS = [
+
+    {
+        key: "Data",
+        label: "Data"
+    },
+
+    {
+        key: "Hora",
+        label: "Hora"
+    },
+
+    {
+        key: "Empregado / Matrícula",
+        label: "Empregado"
+    },
+
+    {
+        key: "Veículo",
+        label: "Veiculo"
+    },
+
+    {
+        key: "Passageiro / Setor / Motivo",
+        label: "Passageiro"
+    },
+
+    {
+        key: "Itinerário",
+        label: "Itinerário"
+    },
+
+    {
+        key: "Status",
+        label: "Status"
+    }
+
+];
+
+// ============================================================================
+// CARREGAR TABELA
+// ============================================================================
+
+export async function carregarTabela() {
+
+    const lista =
+        await obterLancamentos();
+
+    if (!Array.isArray(lista)) {
+
+        throw new Error(
+            "A API não retornou uma lista de lançamentos."
+        );
+
+    }
+
+                                                    console.log(
+                                                                "ENGINE → LANCAMENTOS:",
+                                                                                            lista
+    );
+
+    definirRegistros(lista);
+
+    const tabela =
+        document.querySelector("#tabelaLancamentos");
+
+    if (!tabela) {
+
+        console.error(
+            "Tabela #tabelaLançamentos não encontrada."
+        );
+
+        return;
+    }
+
+    renderTable(
+        tabela,
+        COLUNAS_LANCAMENTOS,
+        lista,
+        {
+
+      edit: true,
+
+      remove: true
+
+    }
+
+    );
+
+}
