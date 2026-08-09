a874825415e2394d117f93791537cd4c97eeccb8
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-09 15:23:03 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,35 +9,6 @@ import {
 } from "./core/router.js";
 
 
-import {
-    initVeiculos
-} from "./pages/veiculos/veiculos.js";
-
-
-import {
-    initEmpregados
-} from "./pages/empregados/empregados.js";
-
-
-// ============================================================================
-// HANDLERS
-// ============================================================================
-
-const handlers = {
-
-    veiculos:
-        initVeiculos,
-
-    empregados:
-        initEmpregados
-
-};
-
-
-// ============================================================================
-// BOOT
-// ============================================================================
-
 function boot() {
 
     console.log(
@@ -45,23 +16,14 @@ function boot() {
     );
 
 
-    startRouter(
-        handlers,
-        {
-            container:
-                "#app",
+    startRouter({
 
-            viewsPath:
-                "./js/pages"
-        }
-    );
+        container: "#app"
+
+    });
 
 }
 
-
-// ============================================================================
-// DOM READY
-// ============================================================================
 
 document.addEventListener(
     "DOMContentLoaded",
