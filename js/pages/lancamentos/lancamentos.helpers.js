85cb2eee60316cf95600076722e29188c5c6f4e1
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-10 20:08:48 -0300

    Update lancamentos.helpers.js

--- js/pages/lancamentos/lancamentos.helpers.js
+++ js/pages/lancamentos/lancamentos.helpers.js
@@ -43,7 +43,7 @@ export const COLUNAS_LANCAMENTOS = [
 
     {
         key: "Passageiro / Setor / Motivo",
-        label: "Passageiro"
+        label: "Passageiro / Setor / Motivo"
     },
 
     {
