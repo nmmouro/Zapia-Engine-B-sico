d36322b5b086d0d436d37a0117bacbd04b6df2fd
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-09 21:48:39 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,11 +9,44 @@ import {
 } from "./core/router.js";
 
 
+/*
+function atualizarRelogio() {
+
+    const agora = new Date();
+
+    const clock =
+        document.getElementById("clock");
+
+    if (!clock) return;
+
+    clock.textContent =
+        agora.toLocaleString(
+            "pt-BR",
+            {
+                dateStyle: "short",
+                timeStyle: "medium"
+            }
+        );
+}
+
+*/
+
+
 function boot() {
 
     console.log(
         "ENGINE → Boot"
     );
+
+
+    /*
+    atualizarRelogio();
+
+    setInterval(
+        atualizarRelogio,
+        1000
+    );
+    */
 
 
     startRouter({
