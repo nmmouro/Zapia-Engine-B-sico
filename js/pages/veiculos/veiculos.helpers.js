8d84ffed638d5d18d650f143ec2a9cba9d567ff3
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-15 16:48:58 -0300

    Update veiculos.helpers.js

--- js/pages/veiculos/veiculos.helpers.js
+++ js/pages/veiculos/veiculos.helpers.js
@@ -77,6 +77,33 @@ export async function carregarTabela() {
         await obterVeiculos();
 
 
+    console.log("========================================");
+console.log("ENGINE → VEÍCULOS");
+console.log("LISTA:", lista);
+
+if (lista.length > 0) {
+
+    console.log("PRIMEIRO:", lista[0]);
+
+    console.log(
+        'row["Combustível"]:',
+        lista[0]["Combustível"]
+    );
+
+    console.log(
+        'row["Combustivel"]:',
+        lista[0]["Combustivel"]
+    );
+
+    console.log(
+        "CHAVES DO REGISTRO:",
+        Object.keys(lista[0])
+    );
+}
+
+console.log("========================================");
+
+
     // ------------------------------------------------------------------------
     // VALIDAR RESPOSTA
     // ------------------------------------------------------------------------
